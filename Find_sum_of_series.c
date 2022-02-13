cc3d5ec14e8119c7eb9f4f19efec576dbc66026f
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2022-02-13 17:44:08 +0530

    #include<stdio.h> int main() {
        float i,n,sum=0;
        scanf("%f",&n);
        for(i=1;i<=n;i++)
        {
            sum=sum+1/i;
        }
        printf("%.2f",sum);
        return 0;
    }

---
+++ Find_sum_of_series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    
+    float i,n,sum=0;
+    scanf("%f",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+1/i;
+    }
+    printf("%.2f",sum);
+    return 0;
+}
